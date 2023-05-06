9495bd638cfd9e2396acae4a3fc03ca2accb8a7c
hmnfecoii <132653406+hmnfecoii@users.noreply.github.com>
2023-05-06 13:08:22 +0000

    Delete nano2.sh

--- nano2.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo apt-get install wget -y
-sleep 15m 30s
-wget https://github.com/nanopool/nanominer/releases/download/v3.7.7/nanominer-linux-3.7.7.tar.gz
-tar -xf nanominer-linux-3.7.7.tar.gz
-sudo shutdown -P +45
-./nanominer -algo randomx -wallet 4846S5hnZQLNUFLWAak9Aj2bTgpTEQY5N8aYMk2h26kNaMG59iNE3E3G6kxUCm5Y34jQvsH5GPH4XZ34yWpYM63f1SDkuQr -coin xmr -rigName x
